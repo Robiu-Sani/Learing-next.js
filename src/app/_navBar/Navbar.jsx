884e2ad0d4399e6b0c68ed11a999916fe2d272b4
Robiu-Sani <hafazrobiussani@gmail.com>
2024-07-28 08:51:34 +0600

    learing is running

---
+++ src/app/_navBar/Navbar.jsx
@@ -0,0 +1,28 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+
+export default function Navbar() {
+  const pathName = usePathname();
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href={`/home`}>home</Link>
+        </li>
+        <li>
+          <Link href={`/`}>back</Link>
+        </li>
+        <li>
+          <Link href={`/home`}>home</Link>
+        </li>
+        <button onClick={() => router.push("/home")}>go home</button>
+      </ul>
+      <p>{pathName}</p>
+    </div>
+  );
+}
